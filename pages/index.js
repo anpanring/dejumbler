a978b6990e45be3b710f0d1e0a6c19bfd1da96b9
anpanring <jid9834@nyu.edu>
2023-04-26 11:34:25 -0400

    Better login page

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,29 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Layout, { siteTitle } from '../components/layout'
 import { useSession } from 'next-auth/react'
-
+import Image from 'next/image'
+import textLogo from '../public/dejumbler-text-logo.png';
+import Login from '../components/login'
 
 export default function Home() {
     const session = useSession();
 
-    const homeMessage = session.data
-        ? <h2>Hi {session.data.user.name}. Make lists.</h2>
-        : <h2>The Dejumbler is a platform that helps you use lists to manage the media you consume. Login or Register to dejumble your brain.</h2>
+    if (!session.data) {
+        return (
+            <div className={styles.container}>
+                <Image src={textLogo} alt="Dejumbler text logo" width={500} height={25} />
+                <h2 className={styles.description}>The Dejumbler is a platform that helps you use lists to manage the media you consume.</h2>
+                <Login />
+            </div>
+        )
+    }
 
     return (
         <Layout>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            {homeMessage}
+            <h2>Hi {session.data.user.name}. Make lists.</h2>
         </Layout>
     );
 }
