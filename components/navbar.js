50e6be2bb1d9b6d612d9425e3a0378131aad2791
anpanring <jid9834@nyu.edu>
2024-04-25 00:43:54 -0400

    clean up navbar code, fix bug with current accent color indicator

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Link from "next/link";
 
@@ -11,15 +11,22 @@ import { useSession, signOut } from "next-auth/react";
 const colors = ['red', 'orange', 'yellow', 'green', 'lightblue', 'indigo', 'violet'];
 
 export default function Navbar({ changeMode }) {
+    // can probably refactor to just one state
     const [showModal, setShowModal] = useState(false);
     const [showProfile, setShowProfile] = useState(false);
     const [showSettings, setShowSettings] = useState(false);
+    
     const [accentColor, setAccentColor] = useState('green');
 
-    const { data, status } = useSession();
+    const { data } = useSession();
 
     const buttonRef = useRef();
     const containerRef = useRef();
+
+    // have to do this on client side
+    useEffect(() => {
+        localStorage.getItem('accent') && setAccentColor(localStorage.getItem('accent'));
+    }, [])
 
     function toggleModal() {
         setShowModal(!showModal);
@@ -32,9 +39,10 @@ export default function Navbar({ changeMode }) {
     }
 
     return (
-        <div className={styles.navbarBack}>
+        <nav className={styles.navbarBack}>
             <div className={styles.navbarContainer} ref={containerRef}>
                 <div className={styles.navbar}>
+                    {/* Logo / Dark mode toggler */}
                     <svg onClick={changeMode} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 283.5 283.5" className={styles.smallLogo}>
                         <path
                             d="M0 0v283.5h283.5V0H0Zm221.55 108.55h-16v-40h16v40Z"
@@ -43,23 +51,25 @@ export default function Navbar({ changeMode }) {
                             }}
                         />
                     </svg>
-                    {/* <Link className={styles.link} href="/">
-                        <span className={`material-symbols-outlined ${styles.icon}`}>
-                            home
-                        </span>
-                    </Link> */}
+
+                    {/* All lists button */}
                     <Link className={styles.link} href="/all-lists" aria-label="All Lists">
                         <span className={`material-symbols-outlined ${styles.icon}`}>
                             format_list_bulleted
                         </span>
                     </Link>
+
+                    {/* Profile button */}
                     <span onClick={() => setShowProfile(!showProfile)} className={`material-symbols-outlined ${styles.icon}`}>
                         person
                     </span>
+
+                    {/* Settings button */}
                     <span onClick={() => setShowSettings(!showSettings)} className={`material-symbols-outlined ${styles.icon}`}>
                         settings
                     </span>
                 </div>
+                {/* New list button */}
                 <span
                     onClick={toggleModal}
                     ref={buttonRef}
@@ -67,6 +77,8 @@ export default function Navbar({ changeMode }) {
                     add
                 </span>
             </div>
+
+            {/* New list modal */}
             {showModal && <Modal className="modal" toggleModal={toggleModal}>
                 <form className={styles.form} action="/api/new-list" method="POST">
                     <button className={styles.closeButton} onClick={toggleModal}>X</button>
@@ -91,6 +103,8 @@ export default function Navbar({ changeMode }) {
                     </div>
                 </form>
             </Modal>}
+
+            {/* Profile modal */}
             {showProfile && data &&
                 <Modal toggleModal={() => setShowProfile(!showProfile)}>
                     <div className={styles.loggedInWrapper}>
@@ -99,26 +113,25 @@ export default function Navbar({ changeMode }) {
                     </div>
                 </Modal>
             }
+
+            {/* Settings modal */}
             {showSettings &&
                 <Modal toggleModal={() => setShowSettings(!showSettings)}>
                     <div>
                         <p>Accent Color</p>
                         <div className={styles.colorPicker}>
                             {colors.map((color) => {
-                                if (color === accentColor) {
-                                    return <div key={color} style={{ backgroundColor: color }} className={`${styles.color} ${styles.selectedColor}`} onClick={() => changeAccentColor(color)}></div>
-                                }
-                                return <div key={color} style={{ backgroundColor: color }} className={styles.color} onClick={() => changeAccentColor(color)}></div>
+                                return <div
+                                    key={color}
+                                    style={{ backgroundColor: color }}
+                                    className={color === accentColor ? `${styles.color} ${styles.selectedColor}` : styles.color}
+                                    onClick={() => changeAccentColor(color)}>
+                                </div>
                             })}
                         </div>
-                        {/* <div>
-                            <button onClick={changeMode}>Toggle Theme</button>
-                        </div> */}
                     </div>
                 </Modal>
             }
-
-        </div>
-
+        </nav>
     )
 }
