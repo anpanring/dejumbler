0331eef6c02f31eab96c85ed4ee8f293734cc335
anpanring <jid9834@nyu.edu>
2025-04-18 16:44:00 -0700

    building out tests: list container

--- components/all-lists/__tests__/list-container.test.tsx
+++ components/all-lists/__tests__/list-container.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { ListContainer } from '../list-container';
 import { CurrentListContextType, ListData } from '@/types/dejumbler-types';
@@ -10,6 +11,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const mockSetCurrentList = jest.fn();
 const mockSetListData = jest.fn();
+const mockListItem = {
+  __t: 'Album',
+  _id: '664e3b7dc2899c9861398ff2',
+  artURL: 'https://i.scdn.co/image/ab67616d0000b2732590c2d33bd70c8bf059591b',
+  artist: 'Haley Heynderickx',
+  name: 'I Need to Start a Garden',
+  status: 'todo',
+};
 const mockListData = [
   {
     user: 'test',
@@ -19,7 +28,7 @@ const mockListData = [
     _id: '12345',
     createdAt: '2023-01-01',
     slug: 'test-list',
-    items: [{}, {}],
+    items: [mockListItem, mockListItem],
     __v: 0,
   },
 ];
@@ -30,7 +39,7 @@ const mockListMetadata = {
 };
 const queryClient = new QueryClient();
 
-describe('ListBox', () => {
+describe('ListContainer', () => {
   it('renders', () => {
     // Act
     render(
@@ -49,5 +58,31 @@ describe('ListBox', () => {
     // Assert
     expect(screen.getByText('Test List')).toBeInTheDocument();
     expect(screen.getByText('This is a test list')).toBeInTheDocument();
+  });
+
+  it('open list', async () => {
+    const user = userEvent.setup();
+
+    // Act
+    render(
+      <QueryClientProvider client={queryClient}>
+        <CurrentListContext.Provider
+          value={{
+            currentList: mockListMetadata,
+            setCurrentList: mockSetCurrentList,
+          }}
+        >
+          <ListContainer lists={mockListData} setListData={mockSetListData} />
+        </CurrentListContext.Provider>
+      </QueryClientProvider>,
+    );
+
+    // Assert
+    const listLink = screen.getByText('Test List');
+    expect(listLink).toBeInTheDocument();
+    expect(screen.getByText('This is a test list')).toBeInTheDocument();
+
+    await user.click(listLink);
+    expect(mockSetCurrentList).toHaveBeenCalledWith(mockListMetadata);
   });
 });
