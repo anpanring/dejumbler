0daba6827fe873d233c37968b228f5f14c6f4cd4
anpanring <jid9834@nyu.edu>
2025-04-23 22:18:49 -0700

    try server-side auth check

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,43 +10,42 @@ import styles from '@/styles/Home.module.css';
 
 import { useSession } from 'next-auth/react';
 import Loading from '@/components/loading';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth/next';
 
 export default function Home() {
   // check session (client-side)
-  const session = useSession();
+  // const session = useSession();
 
   const router = useRouter();
 
   // loading state - maybe use Suspense instead?
-  if (session.status === 'loading') {
-    return (
-      <>
-        <Head>
-          <title>{siteTitle}</title>
-          <meta
-            name="description"
-            content="Clean your brain out with the Dejumbler."
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta
-            name="google-site-verification"
-            content="n5dZdc1QljJ4k39BSCkZAbhnJS5CjIdAo6OHVqD_c-Y"
-          />
-        </Head>
-        <Loading />
-      </>
-    );
-  }
+  // if (session.status === 'loading') {
+  //   return (
+  //     <>
+  //       <Head>
+  //         <title>{siteTitle}</title>
+  //         <meta
+  //           name="description"
+  //           content="Clean your brain out with the Dejumbler."
+  //         />
+  //         <meta name="og:title" content={siteTitle} />
+  //         <meta name="twitter:card" content="summary_large_image" />
+  //         <meta
+  //           name="google-site-verification"
+  //           content="n5dZdc1QljJ4k39BSCkZAbhnJS5CjIdAo6OHVqD_c-Y"
+  //         />
+  //       </Head>
+  //       <Loading />
+  //     </>
+  //   );
+  // }
 
   // logged out page
-  if (!session.data) {
-    document.documentElement.setAttribute('data-theme', 'light');
-    document.documentElement.style.setProperty('--accent-color', 'green');
-    document.documentElement.style.setProperty(
-      '--secondary-font',
-      'sans-serif',
-    );
+  // if (!session.data) {
+    // document.documentElement.setAttribute('data-theme', 'light');
+    // document.documentElement.style.setProperty('--accent-color', 'green');
+    // document.documentElement.style.setProperty('--secondary-font', 'Epilogue');
     return (
       <>
         <Head>
@@ -62,13 +61,13 @@ export default function Home() {
             content="n5dZdc1QljJ4k39BSCkZAbhnJS5CjIdAo6OHVqD_c-Y"
           />
         </Head>
-        <div className={styles.container}>
+        <div className="flex flex-col items-center justify-center h-screen gap-2">
           <Image
             src={textLogo}
             className={styles.textLogo}
             alt="Dejumbler text logo"
           />
-          <h2 className={styles.description}>
+          <h2 className="text-base text-center">
             The Dejumbler is a platform that helps you use lists to manage the
             media you consume.
           </h2>
@@ -76,34 +75,34 @@ export default function Home() {
         </div>
       </>
     );
-  } else {
-    router.push('/all-lists');
-    return (
-      <>
-        <Head>
-          <title>{siteTitle}</title>
-          <meta
-            name="description"
-            content="Clean your brain out with the Dejumbler."
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta
-            name="google-site-verification"
-            content="n5dZdc1QljJ4k39BSCkZAbhnJS5CjIdAo6OHVqD_c-Y"
-          />
-        </Head>
-        <Loading />
-      </>
-      // <Layout>
-      //     <Head>
-      //         <title>Home - Dejumbler</title>
-      //     </Head>
-      //     <h2>Hi {session.data.user.name}!</h2>
-      //     <h3>Your Recent Lists</h3>
-      // </Layout>
-    );
-  }
+  // } else {
+  //   router.push('/all-lists');
+  //   return (
+  //     <>
+  //       <Head>
+  //         <title>{siteTitle}</title>
+  //         <meta
+  //           name="description"
+  //           content="Clean your brain out with the Dejumbler."
+  //         />
+  //         <meta name="og:title" content={siteTitle} />
+  //         <meta name="twitter:card" content="summary_large_image" />
+  //         <meta
+  //           name="google-site-verification"
+  //           content="n5dZdc1QljJ4k39BSCkZAbhnJS5CjIdAo6OHVqD_c-Y"
+  //         />
+  //       </Head>
+  //       <Loading />
+  //     </>
+  //     // <Layout>
+  //     //     <Head>
+  //     //         <title>Home - Dejumbler</title>
+  //     //     </Head>
+  //     //     <h2>Hi {session.data.user.name}!</h2>
+  //     //     <h3>Your Recent Lists</h3>
+  //     // </Layout>
+  //   );
+  // }
 
   // logged in page
   // return (
@@ -118,19 +117,20 @@ export default function Home() {
 }
 
 // check login server-side to prevent flashing login page
-// export async function getServerSideProps(context) {
-//     const session = await getServerSession(context.req, context.res, authOptions);
-//     if (session) {
-//         return {
-//             redirect: {
-//                 destination: '/all-lists',
-//                 permanent: true,
-//             },
-//         }
-//     }
-//     return {
-//         props: {
-//             session: session
-//         }
-//     }
-// }
+export async function getServerSideProps(context) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  if (session) {
+    return {
+      redirect: {
+        destination: '/all-lists',
+        permanent: true,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session: session,
+    },
+  };
+}
